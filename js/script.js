b3f22b8526fb8da1e3ac8a7ce64494a0618c3ec0
Matías Lucas Martínez <matiaslucasmartinez2@gmail.com>
2023-07-24 12:08:06 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+let perfil;
+
+for(let i=0; i<3; i++){
+    perfil = prompt(`Seleccione el número del perfil adecuado
+                    1. Administrador
+                    2. Coordinador
+                    3. Cliente`);
+        switch(perfil) {
+            case "1":
+                window.location.href="./pages/IndexAdministrador.html";
+            break;
+            case "2":
+                window.location.href="./pages/IndexCoordinador.html";
+            break;
+            case "3":
+                window.location.href="./pages/IndexCliente.html";
+            break;
+            default:
+            
+            alert("No ha ingresado una opción correcta");
+            break;
+        } 
+        console.log(i);
+}
+console.log("¡¡¡Se ha denegado el acceso!!!")
+
+
